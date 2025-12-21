4a3885e24d8993dd9b38038f062e36b2930a162a
WebWizard <thewebwizard00@gmail.com>
2025-12-21 20:16:18 +0000

    I tried setting up a Localnet environment for FlowForge using Hardhat, b

---
+++ hardhat.config.ts
@@ -0,0 +1,11 @@
+
+import { HardhatUserConfig } from "hardhat/config";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+};
+
+export default config;
